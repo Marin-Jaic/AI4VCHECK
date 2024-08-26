349cb0a09f6f11909e5814a0638bf1a6cc4710ad
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-08-26 19:09:17 +0200

    improved segmentation

--- segment.py
+++ segment.py
@@ -1,16 +1,17 @@
+import os
+import sys
 from typing import Optional
 
 import cv2 as cv
 import numpy as np
-import sys
 
 
-def segment_TB_pixels(
+def find_contours_TB_pixels(
     img: np.ndarray,
     gray_img: Optional[np.ndarray] = None,
-) -> tuple[np.ndarray, float]:
-    """Segments the Trypan Blue-positive (TB) pixels from the corneal image (with the
-    cornea already segmented out) via the Watershed algorithm.
+) -> tuple[tuple[np.ndarray], np.ndarray, float, tuple]:
+    """Finds the contours of the Trypan Blue-stained (TB) regions in the corneal image
+    (with the cornea already segmented out) via the Watershed algorithm.
 
     Parameters
     ----------
@@ -24,18 +25,31 @@ def segment_TB_pixels(
 
     Returns
     -------
-    tuple of array and float
-        The watershed-segmented regions that are believed to be stained by the TB dye,
-        and the viability index as the ratio of number of healthy pixels (i.e.,
-        TB-negative) to the total number of pixels in the cornea.
+    Contours, mask, and index
+        The watershed-segmented contours of the regions that are believed to be stained
+        by the TB dye (as a tuple of arrays), the TB-positive mask image (as an array),
+        and the viability index as the ratio of number of healthy pixels
+        (i.e., TB-negative) to the total number of pixels in the cornea (a float).
     """
-    # convert to grayscale, remove noise and smooth image, and apply a first threshold
-    # to coarsely extract all suspected TB-positive pixels
+    # first of all, convert to grayscale and compute the mask of the cornea (i.e.,
+    # separate pixels within the cornea from pixels outside of it)
     has_four_channels = img.shape[2] == 4
     if gray_img is None:
         gray_img = cv.cvtColor(
             img, cv.COLOR_BGRA2GRAY if has_four_channels else cv.COLOR_BGR2GRAY
         )
+    corneal_mask = np.where(
+        img[..., -1] > 0 if has_four_channels else gray_img > 0,
+        np.uint8(255),
+        np.uint8(0),
+    )
+
+    # convert to grayscale, remove noise and smooth image, and apply a first threshold
+    # to coarsely extract all suspected TB-positive pixels
+    if False:  # NOTE: tunable (!!!) - bool: perform CLAHE or not
+        size = min(img.shape[:2]) // 100
+        clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(size, size))
+        gray_img = clahe.apply(gray_img)
     blurred_img = cv.GaussianBlur(
         gray_img,
         (9, 9),  # NOTE: tunable (!) - pairs of positive integers
@@ -49,6 +63,12 @@ def segment_TB_pixels(
         101,  # NOTE: tunable (!!) - odd integer
         3.0,  # NOTE: tunable (!!!) - float
     )
+    # _, axs = plt.subplots(1, 2, constrained_layout=True, sharex=True, sharey=True)
+    # axs[0].imshow(blurred_img, cmap="gray")
+    # axs[1].imshow(thresholded_img, cmap="gray")
+    # for ax in axs.flat:
+    #     ax.set_axis_off()
+    # plt.show()
 
     # segment the image via the Watershed algorithm, according to the tutorial found at
     # https://docs.opencv.org/3.4/d3/db4/tutorial_py_watershed.html.
@@ -58,7 +78,15 @@ def segment_TB_pixels(
         cv.MORPH_RECT,
         (3, 3),  # NOTE: tunable (!!) - pairs of positive integers (odd?)
     )
-    mask = cv.morphologyEx(thresholded_img, cv.MORPH_OPEN, kernel)
+    mask = cv.morphologyEx(
+        thresholded_img,
+        cv.MORPH_OPEN,
+        kernel,
+        iterations=1,  # NOTE: tunable (!) - positive integer
+    )
+    # plt.imshow(mask, cmap="gray")
+    # plt.axis("off")
+    # plt.show()
 
     # then, for each nonzero pixel compute the distance to the nearest zero pixel, and
     # use it to discern foreground, background and unknown pixels
@@ -72,15 +100,28 @@ def segment_TB_pixels(
     foreground = foreground.astype(np.uint8)
     background = cv.dilate(
         mask,
-        kernel=kernel,
+        kernel,
         iterations=3,  # NOTE: tunable (?) - nonnegative integer
     )
     unknown = cv.subtract(background, foreground)
+    # _, axs = plt.subplots(2, 2, constrained_layout=True, sharex=True, sharey=True)
+    # axs[0, 0].imshow(distance, cmap="gray")
+    # axs[0, 1].imshow(foreground, cmap="gray")
+    # axs[1, 0].imshow(background, cmap="gray")
+    # axs[1, 1].imshow(unknown, cmap="gray")
+    # for ax in axs.flat:
+    #     ax.set_axis_off()
+    # plt.show()
 
-    # finally, apply the watershed algorithm with the foreground as seed
+    # finally, apply the watershed algorithm with the foreground as seed. Before it, we
+    # forcefully set the labels for all noncorneal pixels to be the same - this helps
+    # with small edges around, e.g., reflections. As usual, we also set the unknown
+    # regions to zero
     _, labels = cv.connectedComponents(foreground)
-    labels = labels + 1  # shift all the labels up by one
-    labels[unknown > 0] = 0  # and assign label 0 to the unknown region
+    labels = labels + 1
+    labels[unknown > 0] = 0
+    assert corneal_mask[0, 0] == 0, "top-left pixel belongs to the cornea!"
+    labels[corneal_mask == 0] = labels[0, 0]
     markers = cv.watershed(img[..., :3], labels)  # remove alpha channel if present
     # segmentation = img[..., :3].copy()
     # segmentation[markers == -1] = [255, 0, 0]  # boundaries are marked with -1
@@ -97,37 +138,27 @@ def segment_TB_pixels(
         markers.astype(np.uint8), 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU
     )
 
-    # compute the mask associated to only those pixels belonging to the cornea
-    corneal_mask = img[..., -1] > 0 if has_four_channels else gray_img > 0
-    contours_mask = np.where(corneal_mask, contours_mask, np.uint8(0))
-    # corneal_mask = np.where(
-    #     img[..., -1] > 0 if has_four_channels else gray_img > 0,
-    #     np.uint8(255),
-    #     np.uint8(0),
-    # )
-    # size = min(corneal_mask.shape) // 100
-    # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (size, size))
-    # shrunk_corneal_mask = cv.erode(corneal_mask, kernel)
-    # contours_mask = cv.bitwise_and(
-    #     contours_mask, contours_mask, mask=shrunk_corneal_mask
-    # )
-
     # finally, find all the contours (hierarchy information is disregarded)
-    contours, _ = cv.findContours(contours_mask, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    corneal_contours_mask = cv.bitwise_and(contours_mask, corneal_mask)
+    contours, _ = cv.findContours(
+        corneal_contours_mask, cv.RETR_LIST, cv.CHAIN_APPROX_NONE
+    )
 
     # create a mask by filling all found contours, thus highlighting all the pixels that
-    # we believe to be positive to the TB stain
+    # we believe to be positive to the TB stain. Just the make sure, set once again to
+    # zero all the pixels that are outside the cornea
     tb_positive_mask = np.zeros(img.shape[:2], np.uint8)
     for i in range(len(contours)):
         cv.drawContours(tb_positive_mask, contours, i, 255, cv.FILLED)
+    tb_positive_mask = cv.bitwise_and(tb_positive_mask, corneal_mask)
     # img[tb_positive_mask > 0] = (255, 0, 0, 255) if has_four_channels else (255, 0, 0)
     # plt.imshow(cv.cvtColor(img, cv.COLOR_BGRA2RGBA))
     # plt.axis("off")
     # plt.show()
 
     # lastly, compute the viability index
-    viability = 1 - cv.countNonZero(tb_positive_mask) / corneal_mask.sum()
-    return tb_positive_mask, viability
+    viability = 1 - cv.countNonZero(tb_positive_mask) / cv.countNonZero(corneal_mask)
+    return contours, tb_positive_mask, viability
 
 
 def calculate_enclosing_circle(gray_img: np.ndarray) -> tuple[int, int, int]:
@@ -152,27 +183,11 @@ def calculate_enclosing_circle(gray_img: np.ndarray) -> tuple[int, int, int]:
     assert len(cnts) == 1, "Expected only one contour."
 
     (x, y), r = cv.minEnclosingCircle(cnts[0])
-    x, y, r = int(x), int(y), int(r)
-    # cv.circle(img, (x, y), r // 4, (255, 0, 0), 2)
-    # cv.circle(img, (x, y), r // 2, (255, 0, 0), 2)
-    # cv.circle(img, (x, y), r * 3 // 4, (255, 0, 0), 2)
-    # cv.circle(img, (x, y), r, (255, 0, 0), 2)
-    # cv.circle(img, (x, y), 7, (0, 0, 0), -1)
-    # cv.putText(
-    #     img,
-    #     "center",
-    #     (x - 20, y - 20),
-    #     cv.FONT_HERSHEY_SIMPLEX,
-    #     1.5,
-    #     (0, 0, 0),
-    #     2,
-    # )
-    # plt.imshow(img)
-    # plt.show()
-    return x, y, r
+    return int(x), int(y), int(r)
 
 
 if __name__ == "__main__":
+    # load image
     if len(sys.argv) != 2:
         print("Usage: python segment.py <path_to_image>")
         exit(1)
@@ -181,9 +196,37 @@ if __name__ == "__main__":
     if img is None:
         print("Could not open or find the image:", path)
         exit(1)
+
+    # convert to grayscale and segment
     gray_img = cv.cvtColor(
         img, cv.COLOR_BGRA2GRAY if img.shape[2] == 4 else cv.COLOR_BGR2GRAY
     )
-    tb_positive_mask, viability = segment_TB_pixels(img, gray_img)
-    # circles = calculate_enclosing_circle(gray_img)
+    tb_contours, tb_positive_mask, viability = find_contours_TB_pixels(img, gray_img)
     print(path, "- viability Index:", viability)
+
+    # plot image
+    import matplotlib.pyplot as plt
+
+    tb_color = [255, 0, 0]
+    ring_color = [0, 0, 255]
+    if img.shape[2] == 4:
+        tb_color.append(255)
+        ring_color.append(255)
+    thickness = min(img.shape[:2]) * 3 // 1000
+    for i in range(len(tb_contours)):
+        cv.drawContours(img, tb_contours, i, tb_color, thickness, cv.LINE_AA)
+    x, y, r = calculate_enclosing_circle(gray_img)
+    cv.circle(img, (x, y), r // 4, ring_color, thickness, cv.LINE_AA)
+    cv.circle(img, (x, y), r // 2, ring_color, thickness, cv.LINE_AA)
+    cv.circle(img, (x, y), r * 3 // 4, ring_color, thickness, cv.LINE_AA)
+    cv.circle(img, (x, y), r, ring_color, thickness, cv.LINE_AA)
+    cv.circle(img, (x, y), r // 100, ring_color, cv.FILLED, cv.LINE_AA)
+    plt.imshow(cv.cvtColor(img, cv.COLOR_BGRA2RGBA))
+    plt.axis("off")
+    plt.show()
+
+    # save segmented image
+    base_name, ext = os.path.splitext(os.path.basename(path))
+    new_base_name = base_name + " (segmented)"
+    new_path = os.path.join(os.path.dirname(path), new_base_name + ext)
+    cv.imwrite(new_path, img)
